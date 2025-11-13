8b05ed900b1c05e5ba495889508ce4390a70f58b
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-13 21:27:40 +0200

    staging the tokenizer

---
+++ src/lifecycle/tokenizer/staging.py
@@ -0,0 +1,24 @@
+## lets register the model on mlflow here:
+
+### start first by import the necessary libraries first:
+import mlflow
+from configurations_mlflow import set_mlflow_host,set_mlflow_exp
+from configurations_mlflow import load_tokenizer_name,mlflow_client
+
+## now set the mlflow_host and mlflow created experiment within the workflow:
+
+set_mlflow_host()
+set_mlflow_exp()
+
+## create the model + model name variables:
+tokenizer_name=load_tokenizer_name()
+tokenizer_version="1"
+
+## create the mlflow client:
+client=mlflow_client()
+
+client.transition_model_version_stage(
+    name=tokenizer_name,
+    version=tokenizer_version,
+    stage="staging"
+)
