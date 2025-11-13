dfa391864de2c7d70f141d34fd0db51a87c0c7f3
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-13 21:34:58 +0200

    test staging

---
+++ src/lifecycle/model/test_staging.py
@@ -0,0 +1,18 @@
+## test if the model has been staged to the production phase successfully or not:
+from configurations_mlflow import set_mlflow_exp,set_mlflow_host
+from 
+configurations_mlflow import load_model_name,test_model_versioning
+
+
+## set the mlflow experiment and host within the mlflow workflow:
+set_mlflow_exp()
+set_mlflow_host()
+
+
+## create the model's variables:
+model_name=load_model_name()
+model_stage="staging"
+
+test_stage_model=test_model_versioning(name=model_name,stage=model_stage)
+
+print(test_stage_model)
