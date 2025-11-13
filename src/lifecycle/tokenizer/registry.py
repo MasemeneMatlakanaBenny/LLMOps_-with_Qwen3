7ebe0c465fc0518e2cacb205babdbafd2b7d7c8e
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-13 21:26:15 +0200

    added content for tokenizer registry

--- src/lifecycle/tokenizer/registry.py
+++ src/lifecycle/tokenizer/registry.py
@@ -1,1 +1,23 @@
 
+## lets register the model on mlflow here:
+
+### start first by import the necessary libraries first:
+import mlflow
+from configurations_mlflow import set_mlflow_host,set_mlflow_exp,load_tokenizer_name
+from configurations import load_tokenizer
+
+## now set the mlflow_host and mlflow created experiment within the workflow:
+
+set_mlflow_host()
+set_mlflow_exp()
+
+## create the model + model name variables:
+tokenizer=load_tokenizer()
+
+tokenizer_name=load_tokenizer_name
+
+## register the model on the mlflow created experiment:
+run_name="tokenizer_registry"
+
+with mlflow.start_run(run_name=run_name) as run:
+    mlflow.pyfunc.log_model(tokenizer,registered_model_name=tokenizer_name)
